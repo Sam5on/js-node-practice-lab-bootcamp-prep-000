702dbd058ce4122fa535508cc33b2738a845c9ee
samantha <sstephens614@yahoo.com>
2018-05-25 08:28:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Samson"
-var height = "51"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1 )
+console.log(eight) + 1 )
